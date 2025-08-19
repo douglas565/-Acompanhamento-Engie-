6c5151c9ca27517eba9a37df0373beb01cf016cb
Douglas Ramos <138054959+douglas565@users.noreply.github.com>
2025-08-18 22:47:53 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,12 @@
 // Copie as informações do seu projeto Firebase aqui!
 
 const firebaseConfig = {
- apiKey: process.env.FIREBASE_API_KEY || "sua_api_key_aqui",
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN || "acompanhamento-de-product.firebaseapp.com",
-  projectId: process.env.FIREBASE_PROJECT_ID || "acompanhamento-de-product",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET || "acompanhamento-de-product.firebasestorage.app",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID || "555489855578",
-  appId: process.env.FIREBASE_APP_ID || "1:555489855578:web:7f6aa79f5984fca45e7063"
+  apiKey: "AIzaSyDWyV39MzgpvJz4y61HgfO2AeKjoXg2IrE",
+  authDomain: "acompanhamento-de-product.firebaseapp.com",
+  projectId: "acompanhamento-de-product",
+  storageBucket: "acompanhamento-de-product.firebasestorage.app",
+  messagingSenderId: "555489855578",
+  appId: "1:555489855578:web:7f6aa79f5984fca45e7063"
 };
 
 // Inicializar Firebase
@@ -47,5 +47,6 @@ let currentUserData = null;
 let allProductions = [];
 let allUsers = [];
 let charts = {};
+
 
 console.log('🔥 Firebase configurado e inicializado!');
